cd63379eba108d5a3d57571979aeec78db29905c
doganbirol13 <doganbirol13@gmail.com>
2018-07-15 21:52:01 +0300

    firstcommit

---
+++ programs/server/packages/jackyqiu_meteor-jquery-nicescroll.js
@@ -0,0 +1,25 @@
+(function () {
+
+/* Imports */
+var Meteor = Package.meteor.Meteor;
+var global = Package.meteor.global;
+var meteorEnv = Package.meteor.meteorEnv;
+
+(function(){
+
+///////////////////////////////////////////////////////////////////////
+//                                                                   //
+// packages/jackyqiu_meteor-jquery-nicescroll/packages/jackyqiu_mete //
+//                                                                   //
+///////////////////////////////////////////////////////////////////////
+                                                                     //
+///////////////////////////////////////////////////////////////////////
+
+}).call(this);
+
+
+/* Exports */
+if (typeof Package === 'undefined') Package = {};
+Package['jackyqiu:meteor-jquery-nicescroll'] = {};
+
+})();
